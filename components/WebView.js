52cc02abbce6a6790ef472b1ef0e52d8b7661873
admin <admin@admins-iMac.local>
2022-11-17 11:38:32 +0700

    check code ios

--- components/WebView.js
+++ components/WebView.js
@@ -14,6 +14,10 @@ import {
   BackHandler,
   Dimensions,
   ActivityIndicator,
+  Modal,
+  SafeAreaView,
+  TouchableOpacity,
+  Image
 } from "react-native"
 
 import VnrLoading from "./VnrLoading/VnrLoading"
@@ -27,10 +31,17 @@ export default class WebViewComponent extends Component {
     this.state = {
       visible: true,
       token: null,
+      modalLeanOnline: {
+        visible: false,
+        uri: null
+      },
+      isCanGoback : false
       //url: null
       //script_Inject : null,
     }
-    this.refWebView = null
+    this.refWebView = null;
+    this.refWebViewLean = null;
+    this.canGoback = null;
     this.handleBackButtonClick = this.handleBackButtonClick.bind(this)
   }
   componentWillMount() {
@@ -108,7 +119,7 @@ export default class WebViewComponent extends Component {
       data = event.nativeEvent.data
     }
 
-    console.log(data,'datadatadata')
+    console.log(data, 'datadatadata')
 
     if (
       data != null &&
@@ -124,15 +135,49 @@ export default class WebViewComponent extends Component {
       else {
         ManageFileSevice.DownloadFile(data.uri)
       }
-    } else {
+    }
+    else if (data != null &&
+      typeof data == "object" &&
+      data.type === "E_LEARNONLINE" &&
+      data.uri !== null &&
+      data.uri !== ""
+    ) {
+      console.log(data.uri, 'data.uridata.uri')
+      this.setState({
+        modalLeanOnline: {
+          visible: true,
+          uri: data.uri
+        }
+      })
+    }
+    else if (event.nativeEvent.data && (data.uri == null && data.uri == "")) {
       await AsyncStorage.setItem("Token", event.nativeEvent.data)
       //this.setState({ token: event.nativeEvent.data })
     }
   }
 
+  closeModalLean = () => {
+    this.setState({
+      modalLeanOnline: {
+        visible: false,
+        uri: null
+      }
+    })
+  }
+
+  goBack = () => {
+    console.log(this.canGoback && this.refWebViewLean,'this.canGoback && this.refWebViewLean')
+    if (this.canGoback && this.refWebViewLean) {
+      this.refWebViewLean.goBack()
+    }
+    else {
+      this.closeModalLean();
+    }
+  }
+
   render() {
     const { DeviceId, Token, DataNotification } = this.props;
-    console.log(`${DeviceId}','${Token}','${DataNotification}`,'ádsad')
+    const { modalLeanOnline } = this.state;
     const script_Inject = `
       setTimeout(function () {
         if(window.location.hash === "#/access/LoginMobile" && LoginMobile && typeof LoginMobile == "function"){
@@ -153,12 +198,62 @@ export default class WebViewComponent extends Component {
           javaScriptEnabled={true} //localStorage.setItem("authorizationData", "{"UserID":24872,"Token":"np9Mu1WT10jOK6NL8kdKQa4ReUCf5oeM","UserName":"vnr","UserCode":"vnr","urlApi":"http://portalapi4edu.vnresource.net/","CenterCode":"vnr","IPInternet":null,"IPLocal":null,"AccessTime":"2020-01-07T16:41:12.007875+07:00","ExpireTime":"2020-01-07T17:11:12.007875+07:00","Status":0,"ClientInfo":{},"UserResource":{}}");
           injectedJavaScript={script_Inject}
           onMessage={(event) => this.listenFromWeb(event)}
-          // sharedCookiesEnabled={true}
+        // sharedCookiesEnabled={true}
         // onNavigationStateChange={(navState) => {
         //   // Keep track of going back navigation within component
         //   console.log(navState, 'navState')
         // }}
         />
+        <Modal visible={modalLeanOnline.visible}>
+          <SafeAreaView style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', position: 'relative' }}>
+
+            <WebView
+              // onLoad={() => this.setState({ visible: false })}
+              ref={(ref) => (this.refWebViewLean = ref)}
+              source={{
+                uri: modalLeanOnline.uri,
+              }}
+              onLoadStart={syntheticEvent => {
+
+              }}
+              onLoadEnd={syntheticEvent => {
+                // hàm gọi khi end load
+                const { nativeEvent } = syntheticEvent;
+                this.canGoback = nativeEvent.canGoBack;
+                if (nativeEvent.canGoBack === false) {
+                  this.setState({
+                    isCanGoback: false
+                  })
+                }else{
+                  this.setState({
+                    isCanGoback: true
+                  })
+                }
+              }}
+            />
+
+            <TouchableOpacity onPress={() => this.goBack()}
+              style={{
+                position: 'absolute',
+                bottom: 20,
+                left: (width / 2) - 15,
+                zIndex: 2
+              }}
+            >
+              <SafeAreaView>
+                <View style={{ width: 30, height: 30, borderRadius: 17.5, backgroundColor: '#ffffff' }}>
+                  {this.state.isCanGoback ? (
+                    <Image source={require('../images/back.png')} style={{ width: 35, height: 35, }} />
+                  ) : (
+                    <Image source={require('../images/Close.png')} style={{ width: 35, height: 35, }} />
+                  )}
+                </View>
+              </SafeAreaView>
+            </TouchableOpacity>
+
+          </SafeAreaView>
+        </Modal>
+
         {this.state.visible && (
           <View
             style={{
